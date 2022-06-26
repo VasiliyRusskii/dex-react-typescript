dfdbdcf1b508632a959c6861e9520770880c59af
G750JW <vsk456654@gmail.com>
2022-06-26 16:34:43 +0300

    Edit authorization

--- src/pages/authorization/components/AuthorizationForm.tsx
+++ src/pages/authorization/components/AuthorizationForm.tsx
@@ -6,45 +6,67 @@ import { ReactComponent as EyeIcon } from "../../../assets/icons/eyeIcon.svg";
 import { ReactComponent as GroupLogin } from "../../../assets/icons/groupLogin.svg";
 import { FormState, UseFormHandleSubmit } from "react-hook-form";
 
-
 export const AuthorizationForm = () => {
-    return (
-        <Wrapper>
-            <Form>
-                <Input
-                    label={"Login"}
-                    placeholder="Enter your username"
-                />
-                <Input
-                    error="Request error"
-                    label={"Password"}
-                    placeholder="Enter your password"
-                    icon={<EyeIcon />}
-                />
-                <Button >Sign In</Button>
-            </Form>
-            <Background>
-                <WrapperGroup>
-                    <GroupLogin/>
-                </WrapperGroup>
-            </Background>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <Form>
+        <Title>Sign In</Title>
+        <Input label={"Login"} placeholder="Enter your username" />
+        <Input label={"Password"} placeholder="Enter your password" icon={<EyeIcon />} />
+        <ButtonWrapper>
+          <Button expanded>Sign In</Button>
+        </ButtonWrapper>
+        <SignUp>
+          Not a member yet? <SignUpSpan>Sign up</SignUpSpan>
+        </SignUp>
+      </Form>
+      <Background>
+        <WrapperGroup>
+          <GroupLogin />
+        </WrapperGroup>
+      </Background>
+    </Wrapper>
+  );
 };
 
-const Wrapper = styled.div`
-    
+const Title = styled.h1`
+  font-weight: 400;
+  color: ${(props) => props.theme.colors.blue};
+  align-items: center;
+  text-align: center;
 `;
+
+const Wrapper = styled.div``;
 
 const Background = styled.div`
-    background: ${props => props.theme.colors.lightBlue};
+  display: none;
+  background: ${(props) => props.theme.colors.lightBlue};
 `;
 
-const WrapperGroup = styled.div`
-    
-`;
+const WrapperGroup = styled.div``;
 
 const Form = styled.div`
-    max-width: 366px;
-    
+  margin-left: 24px;
+  margin-right: 24px;
+`;
+
+const ButtonWrapper = styled.div`
+  margin-top: 24px;
+`;
+
+const SignUp = styled.div`
+  color: ${(props) => props.theme.colors.grey};
+  font-size: 15px;
+  font-weight: 500;
+  line-height: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 24px;
+  width: 100%;
+`;
+
+const SignUpSpan = styled.span`
+  color: red;
+  text-decoration: underline;
 `;
